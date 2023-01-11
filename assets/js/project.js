d15201bb56e03aac0fe4dbc2b9ea11a05a0dd879
Zoe Zhao <sevenmilejourney@gmail.com>
2023-01-11 15:19:02 -0500

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -8,14 +8,14 @@ const projects = [
   {
     title: "Extra-Platform Intermediation in Chinese Video Game Services",
     cardImage: "assets/images/project-page/netlikecontrol.jpg",
-    description: "A service supply chain mapping of live streaming, paid boosting and game companionship through in depth interviews and ethnography.",
+    description: "A service supply chain mapping of live streaming, paid boosting and game companionship.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "https://docs.google.com/presentation/d/1yCB2fjlxWz_hjjrByc5xpESX6IrtVxPaA88CVciuzo4/edit?usp=sharing",
   },
   {
     title: "Gendered Discipline of Platform Game Work in China",
-    cardImage: "assets/images/project-page/chess.jpg",
-    description: "A longitudinal study of how gender affects the ways users and workers navigate and interact with digital gaming platforms and cybercafes.",
+    cardImage: "assets/images/project-page/gender.jpg",
+    description: "A longitudinal study of gender and digital gaming platforms.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "https://docs.google.com/presentation/d/1MpVbmgXBoFY6W1-JaZuArdaoLl3Pjyn7SCXKGotBGbU/edit?usp=sharing",
    
@@ -23,9 +23,9 @@ const projects = [
  
   {
     title: "Liquid Dependencies Game",
-    cardImage: "assets/images/project-page/minesweeper.jpg",
+    cardImage: "assets/images/project-page/ldt.jpg",
     description:
-      "A live-action role playing (LARP) game on labor, gender, population aging and care work.",
+      "A live-action role playing game on labor, gender, population aging and care work.",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "https://www.instagram.com/liquiddependencies/?hl=en",
@@ -42,7 +42,7 @@ const projects = [
   },
   {
     title: "Coronavirus Diaries Overseas",
-    cardImage: "assets/images/project-page/minesweeper.jpg",
+    cardImage: "assets/images/project-page/diary.jpg",
     description:
       "An incomplete coronavirus diary archive by the Chinese diaspora in the first half of 2020.",
     tagimg:
