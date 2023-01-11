b8695c6c0c74be5f73380240b63076bc56c8de7e
Zoe Zhao <sevenmilejourney@gmail.com>
2023-01-11 15:31:26 -0500

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -8,14 +8,14 @@ const projects = [
   {
     title: "Extra-Platform Intermediation in Chinese Video Game Services",
     cardImage: "assets/images/project-page/netlikecontrol.jpg",
-    description: "A service supply chain mapping of live streaming, paid boosting and game companionship.",
+    description: "A service supply chain mapping of live streaming, paid boosting and game companionship",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "https://docs.google.com/presentation/d/1yCB2fjlxWz_hjjrByc5xpESX6IrtVxPaA88CVciuzo4/edit?usp=sharing",
   },
   {
     title: "Gendered Discipline of Platform Game Work in China",
     cardImage: "assets/images/project-page/gender.jpeg",
-    description: "A longitudinal study of gender and digital gaming platforms.",
+    description: "A longitudinal study of gender and digital gaming platforms",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "https://docs.google.com/presentation/d/1MpVbmgXBoFY6W1-JaZuArdaoLl3Pjyn7SCXKGotBGbU/edit?usp=sharing",
    
@@ -25,7 +25,7 @@ const projects = [
     title: "Liquid Dependencies Game",
     cardImage: "assets/images/project-page/ldt.jpg",
     description:
-      "A live-action role playing game on labor, gender, population aging and care work.",
+      "A live-action role playing game on labor, gender, population aging and care work",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "https://www.instagram.com/liquiddependencies/?hl=en",
@@ -34,7 +34,7 @@ const projects = [
   {
     title: "Penn Activism, Media and Education Group",
     cardImage: "assets/images/project-page/ameg.jpg",
-    description: "A graduate student interest group on media activism and critical pedagogy.",
+    description: "A graduate student interest group on media activism and critical pedagogy",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "https://www.instagram.com/pennameg/",
@@ -56,7 +56,7 @@ const projects = [
 // function for rendering project cards data
 const showCards = () => {
   let output = "";
-  projects.forEach(({ title, cardImage, Previewlink }) => {
+  projects.forEach(({ title, cardImage, description, Previewlink }) => {
     output += `       
         <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
